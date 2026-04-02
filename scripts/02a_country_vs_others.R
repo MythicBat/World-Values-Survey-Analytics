42fbb9f57b30536e2e453b76633e17a8e4aec0f8
Alin516 <amer--24@student.monash.edu>
2026-04-02 14:26:38 +1100

    restarting

--- scripts/02a_country_vs_others.R
+++ scripts/02a_country_vs_others.R
@@ -12,6 +12,18 @@ conf_group_summary <- VC_clean %>%
 
 write.csv(conf_group_summary, "outputs/tables/q2a_confidence_group_means.csv", row.names = FALSE)
 
+nld_means <- conf_group_summary %>% filter(Group == "Netherlands")
+other_means <- conf_group_summary %>% filter(Group == "OtherCountries")
+
+diff_df <- data.frame(
+  Organization = confidence_vars,
+  NetherlandsMean = as.numeric(nld_means[1, confidence_vars]),
+  OtherCountriesMean = as.numeric(other_means[1, confidence_vars])
+)
+
+diff_df$Difference <- diff_df$NetherlandsMean - diff_df$OtherCountriesMean
+diff_df <- diff_df %>% arrange((Difference))
+
 # Mean comparison for predictors
 pred_group_summary <- VC_clean %>%
   group_by(Group) %>%
